ba0bba184799fd3e592810a2a8c280311834ee7f
Jinsoo Yoon <floreo1242@gmail.com>
2025-05-30 04:40:17 +0900

    엔드포인트 경로 중복 수정

--- src/api/company.js
+++ src/api/company.js
@@ -4,22 +4,10 @@ const router = express.Router();
 const companyController = require("../controllers/companyController");
 const { authorize, authMiddleware } = require("../utils/authorize");
 
-router.post("/company", authorize(["admin"]), companyController.createCompany);
-router.get(
-  "/company/:id",
-  authorize(["admin"]),
-  companyController.getCompanyById,
-);
-router.get("/company", authorize(["admin"]), companyController.getAllCompanies);
-router.put(
-  "/company/:id",
-  authorize(["admin"]),
-  companyController.updateCompany,
-);
-router.delete(
-  "/company/:id",
-  authorize(["admin"]),
-  companyController.deleteCompany,
-);
+router.post("/", authorize(["admin"]), companyController.createCompany);
+router.get("/:id", authorize(["admin"]), companyController.getCompanyById);
+router.get("/", authorize(["admin"]), companyController.getAllCompanies);
+router.put("/:id", authorize(["admin"]), companyController.updateCompany);
+router.delete("/:id", authorize(["admin"]), companyController.deleteCompany);
 
 module.exports = router;
--- src/api/product.js
+++ src/api/product.js
@@ -4,22 +4,10 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const { authorize, authMiddleware } = require("../utils/authorize");
 
-router.post("/products", authorize(["admin"]), productController.createProduct);
-router.get(
-  "/products/:id",
-  authorize(["admin"]),
-  productController.getProductById,
-);
-router.get("/products", authorize(["admin"]), productController.getAllProducts);
-router.put(
-  "/products/:id",
-  authorize(["admin"]),
-  productController.updateProduct,
-);
-router.delete(
-  "/products/:id",
-  authorize(["admin"]),
-  productController.deleteProduct,
-);
+router.post("/", authorize(["admin"]), productController.createProduct);
+router.get("/:id", authorize(["admin"]), productController.getProductById);
+router.get("/", authorize(["admin"]), productController.getAllProducts);
+router.put("/:id", authorize(["admin"]), productController.updateProduct);
+router.delete("/:id", authorize(["admin"]), productController.deleteProduct);
 
 module.exports = router;
