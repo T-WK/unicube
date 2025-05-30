708f1d516e7a38335bea75a7ef63a54d3f09449e
Jinsoo Yoon <floreo1242@gmail.com>
2025-05-31 02:43:19 +0900

    authMiddleware에서 경로를 바탕으로 req.company_id에 id 세팅
    - admin인 경우 0

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,7 +1,24 @@
-module.exports = (req, res, next) => {
+const CompanyModel = require("../models/companyModel");
+
+module.exports = async (req, res, next) => {
   const { hash } = req.params;
   if (hash !== "user1" && hash !== "admin")
     return res.status(403).send("Forbidden");
-  req.company_id = hash;
-  next();
+  if (hash === "admin") {
+    req.company_id = "0"; // 컨트롤러에서 req.company_id가 0이면 전체조회 하도록 구현?
+    next();
+    return;
+  }
+  try {
+    const company_id = await CompanyModel.getCompanyIdByAccessToken(hash);
+    if (company_id) {
+      req.company_id = company_id;
+      next();
+    } else {
+      return res.status(404).send("토큰에 해당하는 업체를 찾을 수 없음");
+    }
+  } catch (error) {
+    console.error("업체 반환 중 오류:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 };
