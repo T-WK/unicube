f4a663ecc9ea6a1349815d70554776ba9ed0bc12
whan0767 <whan0767@naver/com>
2025-05-31 11:49:05 +0900

    dev: company ocr html

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -7,4 +7,14 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/index.html")); // index.html 파일을 응답으로 보낸다.
 });
 
+router.get("/company", (req, res) => {
+  //express 앱(app)을 넣고, root directory에 오면,
+  res.sendFile(path.join(__dirname, "../../public/pages/company.html")); // index.html 파일을 응답으로 보낸다.
+});
+
+router.get("/ocr", (req, res) => {
+  //express 앱(app)을 넣고, root directory에 오면,
+  res.sendFile(path.join(__dirname, "../../public/pages/ocr.html")); // index.html 파일을 응답으로 보낸다.
+});
+
 module.exports = router;
